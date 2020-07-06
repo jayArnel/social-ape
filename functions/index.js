8bc1063d4f9f517706ef1ba23811f77e028601b3
Jay Arnel Bilocura <jay.arnel@gmail.com>
2020-07-06 15:05:48 +0800

    [Initial] Create and read data. Formatting response. User registration.

---
+++ functions/index.js
@@ -0,0 +1,109 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const app = require("express")();
+const firebase = require("firebase");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZRaaj1PhB-4FyzMCjCrAqMz5hpxmih2M",
+  authDomain: "social-ape-d65a0.firebaseapp.com",
+  databaseURL: "https://social-ape-d65a0.firebaseio.com",
+  projectId: "social-ape-d65a0",
+  storageBucket: "social-ape-d65a0.appspot.com",
+  messagingSenderId: "528251224183",
+  appId: "1:528251224183:web:bb52afbb378a4a26d30672",
+  measurementId: "G-V41JK2RWST",
+};
+firebase.initializeApp(firebaseConfig);
+
+admin.initializeApp();
+const db = admin.firestore();
+
+app.get("/screams", (req, res) => {
+  db.collection("screams")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        });
+      });
+      return res.json(screams);
+    })
+    .catch((err) => console.error(err));
+});
+
+app.post("/scream", (req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString(),
+  };
+  db.collection("screams")
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
+
+// Sign up
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res
+          .status(400)
+          .json({ handle: "This handle is already taken." });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already in use." });
+      } else {
+        return res
+          .status(500)
+          .json({ general: "Something went wrong, please try again" });
+      }
+    });
+});
+
+exports.api = functions.region("asia-east2").https.onRequest(app);
