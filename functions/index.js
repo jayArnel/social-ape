fb37f30f66fdc3a182feacf7aef77e2c07575c69
Jay Arnel Bilocura <jay.arnel@gmail.com>
2020-07-07 19:03:07 +0800

    Delete screams

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,7 @@ const {
   commentOnScream,
   likeScream,
   unlikeScream,
+  deleteScream,
 } = require("./handlers/screams");
 
 const {
@@ -25,6 +26,7 @@ const {
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
 app.get("/scream/:screamId", getScream);
+app.delete("/scream/:screamId", FBAuth, deleteScream);
 app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
 app.get("/scream/:screamId/like", FBAuth, likeScream);
 app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
