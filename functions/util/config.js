91e2daabd27a1df5df619aeb40533492052e7eaa
Jay Arnel Bilocura <jay.arnel@gmail.com>
2020-07-07 09:41:08 +0800

    Re-organized code

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyAZRaaj1PhB-4FyzMCjCrAqMz5hpxmih2M",
+  authDomain: "social-ape-d65a0.firebaseapp.com",
+  databaseURL: "https://social-ape-d65a0.firebaseio.com",
+  projectId: "social-ape-d65a0",
+  storageBucket: "social-ape-d65a0.appspot.com",
+  messagingSenderId: "528251224183",
+  appId: "1:528251224183:web:bb52afbb378a4a26d30672",
+  measurementId: "G-V41JK2RWST",
+};
