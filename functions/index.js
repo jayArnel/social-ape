a75e3a6ddcaf9f5526f2bd6556b3c3170d27e802
Jay Arnel Bilocura <jay.arnel@gmail.com>
2020-07-08 14:43:04 +0800

    Added database triggers. Update image url on change of profile image. Delete related docs on scream delete. Code improvements.

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,7 @@
 const functions = require("firebase-functions");
-
 const app = require("express")();
-
 const FBAuth = require("./util/fbAuth");
-
 const { db } = require("./util/admin");
-
 const {
   getAllScreams,
   postOneScream,
@@ -44,16 +40,21 @@ app.get("/user", FBAuth, getAuthenticatedUser);
 app.get("/user/:handle", getUserDetails);
 app.post("/notifications", FBAuth, markNotificationsRead);
 
-exports.api = functions.region("asia-east2").https.onRequest(app);
+const region = "asia-east2";
+
+exports.api = functions.region(region).https.onRequest(app);
 
 exports.createNotificationOnLike = functions
-  .region("asia-east2")
+  .region(region)
   .firestore.document("/likes/{id}")
   .onCreate((snapshot) => {
     db.doc(`/screams/${snapshot.data().screamId}`)
       .get()
       .then((doc) => {
-        if (doc.exists) {
+        if (
+          doc.exists &&
+          doc.data().userHandle !== snapshot.data().userHandle
+        ) {
           return db.doc(`/notifications/${snapshot.id}`).set({
             createdAt: new Date().toISOString(),
             recipient: doc.data().userHandle,
@@ -68,20 +69,17 @@ exports.createNotificationOnLike = functions
   });
 
 exports.deleteNotificationOnUnLike = functions
-  .region("asia-east2")
+  .region(region)
   .firestore.document("likes/{id}")
   .onDelete((snapshot) => {
     return db
       .doc(`/notifications/${snapshot.id}`)
       .delete()
-      .catch((err) => {
-        console.error(err);
-        return;
-      });
+      .catch((err) => console.error(err));
   });
 
 exports.createNotificationOnComment = functions
-  .region("asia-east2")
+  .region(region)
   .firestore.document("comments/{id}")
   .onCreate((snapshot) => {
     return db
@@ -102,8 +100,59 @@ exports.createNotificationOnComment = functions
           });
         }
       })
-      .catch((err) => {
-        console.error(err);
-        return;
-      });
+      .catch((err) => console.error(err));
+  });
+
+exports.onUserImageChange = functions
+  .region(region)
+  .firestore.document("/users/{userId}")
+  .onUpdate((change) => {
+    if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+      const batch = db.batch();
+      return db
+        .collection("screams")
+        .where("userHandle", "==", change.before.data().handle)
+        .get()
+        .then((data) => {
+          data.forEach((doc) => {
+            const scream = db.doc(`/screams/${doc.id}`);
+            batch.update(scream, { userImage: change.after.data().imageUrl });
+          });
+          return batch.commit();
+        });
+    } else return true;
+  });
+
+exports.onScreamDelete = functions
+  .region(region)
+  .firestore.document("/screams/{screamId}")
+  .onDelete((snapshot, context) => {
+    const screamId = context.params.screamId;
+    const batch = db.batch();
+    return db
+      .collection("comments")
+      .where("screamId", "==", screamId)
+      .get()
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/comments/${doc.id}`));
+        });
+        return db.collection("likes").where("screamId", "==", screamId).get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/likes/${doc.id}`));
+        });
+        return db
+          .collection("notifications")
+          .where("screamId", "==", screamId)
+          .get();
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/notifications/${doc.id}`));
+        });
+        return batch.commit();
+      })
+      .catch((err) => console.error(err));
   });
