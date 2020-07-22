282eea66995ba2e1432ba4cce13e02ad17e4c0cf
Jay Arnel Bilocura <jay.arnel@gmail.com>
2020-07-22 19:07:12 +0800

    [Test] Configured test environment.

---
+++ functions/tests/validators.test.js
@@ -0,0 +1,57 @@
+const validators = require("../util/validators");
+
+describe("Test signup validator", () => {
+  test("valid signup data", () => {
+    const validUserData = {
+      email: "user@example.com",
+      password: "password",
+      confirmPassword: "password",
+      handle: "user",
+    };
+    const validation = validators.validateSignupData(validUserData);
+    console.log(validation);
+    expect(validation).toEqual({ errors: {}, valid: true });
+  });
+
+  test("empty email", () => {
+    const validUserData = {
+      email: "",
+      password: "password",
+      confirmPassword: "password",
+      handle: "user",
+    };
+    const validation = validators.validateSignupData(validUserData);
+    expect(validation).toEqual({
+      errors: { email: "Must not be empty." },
+      valid: false,
+    });
+  });
+  test("invalid email", () => {
+    const validUserData = {
+      email: "user@e",
+      password: "password",
+      confirmPassword: "password",
+      handle: "user",
+    };
+    const validation = validators.validateSignupData(validUserData);
+    console.log(validation);
+    expect(validation).toEqual({
+      errors: { email: "Must be a valid email address." },
+      valid: false,
+    });
+  });
+
+  test("empty password", () => {
+    const validUserData = {
+      email: "user@email.com",
+      password: "",
+      confirmPassword: "password",
+      handle: "user",
+    };
+    const validation = validators.validateSignupData(validUserData);
+    expect(validation).toEqual({
+      errors: { password: "Must not be empty." },
+      valid: false,
+    });
+  });
+});
